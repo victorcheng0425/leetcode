4f8f78178263545e60031589c0ab0c698898dc99
Victor Cheng <59785657+victorcheng0425@users.noreply.github.com>
2020-07-03 23:44:31 -0700

    Create Valid Sudoku.py

---
+++ Valid Sudoku.py
@@ -0,0 +1,66 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        i = 0
+        j = 0
+        box = 0
+        x = 0
+        while(x < 9):
+            li = [0]*10
+            j = x
+            while(i < 9):
+                if board[i][j] == ".":
+                    i += 1
+                    continue
+                #print(x,i,j)
+                if li[int(board[i][j])] is 1:
+                    print("A", "x: ", x, "i: ", i, "j: ", j)
+                    return False
+                li[int(board[i][j])] = 1
+                i += 1
+            j = 0
+            li = [0]*10
+            i = x
+            while(j < 9):
+                if board[i][j] == ".":
+                    j += 1
+                    continue
+                if li[int(board[i][j])] is 1:
+                    print("b", "x: ", x, "i: ", i, "j: ", j)
+                    return False
+                li[int(board[i][j])] = 1
+                j += 1
+            if x == 0 or x == 3 or x ==6:
+                j = 0
+            elif x == 1 or x == 4 or x == 7:
+                j = 3
+            elif x == 2 or x == 5 or x == 8:
+                j = 6
+            if x == 0 or x == 1 or x == 2:
+                i = 0
+            elif x == 3 or x == 4 or x == 5:
+                i = 3
+            elif x == 6 or x == 7 or x == 8:
+                i = 6
+            temp = j
+            li = [0]*10
+            while(box < 9):
+                if box == 3 or box == 6:
+                    i += 1
+                    j = temp
+                print("d", x,i,j)
+                if board[i][j] == ".":
+                    box += 1
+                    j += 1
+                    continue
+                if li[int(board[i][j])] is 1:
+                    print(li)
+                    return False
+                li[int(board[i][j])] = 1
+                j += 1
+                box += 1
+            box = 0
+            i = 0
+            j = 0
+            x += 1
+        return True
+                
